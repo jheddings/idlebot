ed00e2cdd069972e7754fb510695af3faef65492
jheddings <jason@heddings.com>
2020-01-11 10:21:32 -0700

    Refactored for improved configuration and connection handling

--- src/idlebot.py
+++ src/idlebot.py
@@ -2,7 +2,6 @@
 
 import os
 import time
-import yaml
 
 import logging
 import logging.config
@@ -12,7 +11,22 @@ import irc
 logger = logging.getLogger('IdleBot')
 
 #---------------------------------------------------------------------------
+def parse_args():
+    import argparse
+
+    argp = argparse.ArgumentParser(description='idlebot: a bot for playing IdleRPG')
+
+    argp.add_argument('--config', default='/etc/idlebot.cfg',
+                      help='configuration file (default: /etc/idlebot.cfg)')
+
+    argp.add_argument('params', nargs=argparse.REMAINDER)
+
+    return argp.parse_args()
+
+#---------------------------------------------------------------------------
 def load_config(config_file):
+    import yaml
+
     if not os.path.exists(config_file):
         logging.warning('config file does not exist: %s', config_file)
         return None
@@ -29,11 +43,11 @@ def load_config(config_file):
 
 #---------------------------------------------------------------------------
 def on_welcome(client, msg):
-    client.join(conf['IdleRPG']['Channel'])
+    client.join(idlerpg_conf['Channel'])
 
-    username = conf['IdleRPG']['Username']
-    password = conf['IdleRPG']['Password']
-    faction = conf['IdleRPG']['Faction']
+    username = idlerpg_conf['Username']
+    password = idlerpg_conf['Password']
+    faction = idlerpg_conf['Faction']
 
     # TODO register if needed
     #client.msg('bot', 'REGISTER {0} {1} {2}'.format(username, password, faction))
@@ -41,24 +55,34 @@ def on_welcome(client, msg):
     client.msg('bot', 'LOGIN {0} {1}'.format(username, password))
 
 #---------------------------------------------------------------------------
+def connect(cfg):
+    # configure the client and event handlers
+    nickname = cfg['Nickname']
+    fullname = cfg['FullName']
+
+    client = irc.Client(nickname, fullname)
+    client.on_welcome += on_welcome
+
+    # connect to the IRC server and start to idle...
+    irc_server = cfg['Hostname']
+    irc_port = cfg['Port']
+
+    client.connect(irc_server, port=irc_port)
+
+    return client
+
+#---------------------------------------------------------------------------
 ## MAIN ENTRY
 
-conf = load_config('idlebot.cfg')
+args = parse_args()
+conf = load_config(args.config)
 
 # TODO verify conf contents
+server_conf = conf['Server']
+idlerpg_conf = conf['IdleRPG']
 
-# configure the client and event handlers
-nickname = conf['Server']['Nickname']
-fullname = conf['Server']['FullName']
-
-client = irc.Client(nickname, fullname)
-client.on_welcome += on_welcome
-
-# connect to the IRC server and start to idle...
-irc_server = conf['Server']['Hostname']
-irc_port = conf['Server']['Port']
-
-client.connect(irc_server, port=irc_port)
+# fire up the game...
+client = connect(server_conf)
 
 # wait for user to quit (Ctrl-C)
 try:
