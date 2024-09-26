5b705ff3564602f0a5dcd06e52b41876c3f792d4
jheddings <jason@heddings.com>
2024-09-26 09:38:17 -0600

    Basic parsing test.

---
+++ tests/test_player.py
@@ -0,0 +1,17 @@
+import logging
+import unittest
+
+from idlebot.player import PlayerInfo
+
+# keep logging output to a minumim for testing
+logging.basicConfig(level=logging.FATAL)
+
+
+class PlayerInfoTest(unittest.TestCase):
+    def test_parse_player_info(self):
+        with open("tests/player.xml") as fp:
+            text = fp.read()
+
+        player = PlayerInfo.from_xml(text)
+
+        self.assertEqual(player.username, "test_subject")
